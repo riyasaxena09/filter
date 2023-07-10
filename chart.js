5eb58203771213e5295edae0987c09d1b03ab870
riyasaxena09 <riyasaxenabdn2002@gmail.com>
2023-07-10 22:48:45 +0530

    initial commit

---
+++ chart.js
@@ -0,0 +1,16 @@
+import './barchart.css';
+function barchart(props){
+  let barheight='0%';
+  if(props.max>0){
+    barheight=Math.round((props.value/props.maxValue)*100)+'%';
+  }
+  return(
+    <div className='chart-bar'>
+      <div className='chart-bar__inner'>
+        <div className='chart-bar__fill' style={{height:barheight}}>
+          </div>
+      </div>
+      <div className='chart-bar__label'>{props.label}</div>
+    </div>
+  )
+}
